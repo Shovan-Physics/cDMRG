ee549852a7fc658c11b854cb07f52d3d08f6df8f
Shovan-Physics <88729410+Shovan-Physics@users.noreply.github.com>
2021-08-10 18:42:00 +0100

    Add files via upload

---
+++ Parameters/savetable.m
@@ -0,0 +1,6 @@
+(* Created with the Wolfram Language : www.wolfram.com *)
+{1 -> {"maxpower" -> Infinity, "savefinalmeas" -> True, "savewf" -> False, 
+   "savewfMMA" -> True, "savespcorr" -> True, "savennavg" -> False, 
+   "saveentropy" -> True, "savelocaldms" -> True, 
+   "saveenergyhistory" -> True, "store_local_history" -> False, 
+   "store_local_history_eps" -> False, "savethresh" -> 1.*^-8}}
